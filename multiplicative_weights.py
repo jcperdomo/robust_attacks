82cb161645b5ab08558dbbda4ddf9419ab7f56eb
Juan Perdomo <jcperdomosilva@gmail.com>
2018-12-21 18:14:48 -0500

    updated models, added multiplicative weights file

--- multiplicative_weights.py
+++ multiplicative_weights.py
@@ -1,9 +1,9 @@
 import numpy as np
 import time
-import logging as log
+import torch
 
-def run_mwu(models, iters, X, Y, noise_budget, noise_func, loss, epsilon=None):
 
+def run_mwu(models, iters, X, Y, noise_budget, adversary, epsilon=None):
     num_models = len(models)
     num_points = X.size()[0]
 
@@ -14,15 +14,17 @@ def run_mwu(models, iters, X, Y, noise_budget, noise_func, loss, epsilon=None):
     else:
         delta = 2.0 * epsilon
 
-    log.info("\nRunning MWU for {} Iterations with Epsilon {}\n".format(iters, epsilon))
-    log.info("Guaranteed to be within {} of the minimax value \n".format(delta))
+    print("\nRunning MWU for {} Iterations with Epsilon {}\n".format(iters, epsilon))
+    print("Guaranteed to be within {} of the minimax value \n".format(delta))
 
     # lets first do it for just one point and then add the rest of the functionality for multiple points
     weights = np.ones((num_points, num_models)) / num_models
+    expected_losses = [[] for _ in range(num_points)]
+    minimum_losses = [[] for _ in range(num_points)]
     noise_vectors = []
 
     for t in range(iters):
-        log.debug("Iteration {}\n".format(t))
+        print("Iteration {}\n".format(t))
         start_time = time.time()
 
         # best response is a sequence of m vectors, m=num_points
@@ -30,12 +32,18 @@ def run_mwu(models, iters, X, Y, noise_budget, noise_func, loss, epsilon=None):
 
         for m in range(num_points):
 
-            best_response = adversary(weights, models, X[m], Y[m], noise_budget, noise_func)
-            noise_vectors_t.append(best_response)
+            x = X[m].unsqueeze(0).cuda()
+            y = Y[m].cuda()
+
+            best_response = adversary(weights[m], models, x, y, noise_budget)
 
             # current loss is an array of length num_models
             # TODO
-            current_loss = loss(models, best_response[m], X[m], Y[m])
+            current_loss = np.array([1.0 - model.loss_single(x + best_response, y).item() for model in models])
+            expected_losses[m].append(np.dot(weights[m], current_loss))
+            minimum_losses[m].append(current_loss.min())
+
+            noise_vectors_t.append(best_response.cpu())
 
             # penalize experts
             for i in range(num_models):
@@ -50,25 +58,7 @@ def run_mwu(models, iters, X, Y, noise_budget, noise_func, loss, epsilon=None):
         noise_vectors_t = torch.stack(noise_vectors_t)
         noise_vectors.append(noise_vectors_t)
 
-
-
-
-
-        log.debug("Maximum (Average) Accuracy of Classifier {}".format(acc_history[-1]))
-        if dl:
-            log.debug("Cost (Before Noise) {}".format(np.array([1 - model.evaluate(X, Y, verbose=0)[1] for model in models])))
-        else:
-            log.debug("Cost (Before Noise) {}".format(np.array([1 - model.evaluate(X, Y) for model in models])))
-
-        log.debug("Cost (After Noise), {}".format(cost_t))
-        log.debug("Loss {} Loss Per Action {}".format(loss, individual))
-
-
-
-
-
-
-
-        log.debug("time spent {}\n".format(time.time() - start_time))
-    log.info("finished running MWU ")
-    return w, v, loss_history, acc_history, action_loss
+        print("time spent {}\n".format(time.time() - start_time))
+    noise_vectors = torch.stack(noise_vectors)
+    print("finished running MWU ")
+    return noise_vectors, weights, expected_losses, minimum_losses
